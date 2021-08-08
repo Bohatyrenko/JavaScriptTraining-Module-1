cae6a495e6d1eee7629e80527e0d920bf32feae0
Oleksii Bohatyrenko <steenbras31@gmail.com>
2021-08-08 14:02:34 +0300

    module 1

---
+++ js/1.js
@@ -0,0 +1,31 @@
+//Примеры переменных
+// const BASE_URL = "https//gornot.com/start_page/";
+// const numberSimple = 22;
+// const stringSimple = "string";
+
+// const b = 10;
+// let a = b * 2;
+// console.log("решение уравнения - ", a);
+
+// b = 10;
+// a = b * 2;
+// console.log("решение уравнения без переменных - ", a);
+
+//Пример массива
+// const t = [1, 2, 3, 4, 5, 6, 7, 8];
+// console.log('массив - ', t);
+
+//Применение свойства 'length'
+// const c = "It is const c";
+// const o = c.length;
+// console.log(o);
+
+// const d = c.length;
+// console.log("Длинна переменной с - ", d);
+
+//Применение метода 'toUpperCase()'.
+// const f = c.toUpperCase();
+// console.log(
+//   "Применения метода toUpperCase() - делает все буквы заглавными: ",
+//   f
+// );
